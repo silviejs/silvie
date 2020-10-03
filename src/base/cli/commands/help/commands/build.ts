253efb80ad98df234c0ebf7a1c434599ac810bbc
Hossein Maktoobian <8bitsight@gmail.com>
2020-10-03 18:11:28 +0330

    Create help command

---
+++ src/base/cli/commands/help/commands/build.ts
@@ -0,0 +1,11 @@
+import log from 'base/utils/log';
+
+export default () => {
+	log.warning('SILVIE CLI - Build');
+	log('This command will build the application into the build directory.');
+	log('You need to have a .env file and an assets directory to be copied there.');
+
+	log();
+	log.info('Usage:');
+	log('  silvie build');
+};
---
+++ src/base/cli/commands/help/commands/bundle.ts
@@ -0,0 +1,14 @@
+import log from 'base/utils/log';
+
+export default () => {
+	log.warning('SILVIE CLI - Bundle');
+	log('This command will bundle the application into a single file in bundle directory.');
+	log('You need to have a .env file and an assets directory to be copied there.');
+
+	log();
+	log.warning('Notice!', '.env file will directly loaded into code as hard-coded data.');
+
+	log();
+	log.info('Usage:');
+	log('  silvie bundle');
+};
