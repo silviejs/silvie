6de7319a1ae1d06ca01907d4a7c1dbfcb65f8c55
Hossein Maktoobian <8bitsight@gmail.com>
2020-10-03 15:51:28 +0330

    Create schema make command

---
+++ src/base/cli/commands/make/makers/schema/index.ts
@@ -0,0 +1,35 @@
+import log from 'base/utils/log';
+import { pascalCase, snakeCase } from 'change-case';
+import pluralize from 'pluralize';
+import path from 'path';
+import fs from 'fs';
+
+const template = fs.readFileSync(path.resolve(__dirname, 'template'), { encoding: 'utf8' });
+
+export default (args: { _: string[] }) => {
+	const name = args._[2];
+
+	if (name) {
+		const filename = snakeCase(name);
+		const filepath = path.resolve(process.rootPath, `src/graphql/schemas/${filename}.gql`);
+
+		if (!fs.existsSync(filepath)) {
+			const className = pascalCase(name);
+			const singularName = snakeCase(pluralize(name, 1));
+			const pluralName = snakeCase(pluralize(name));
+
+			const content = template
+				.replace(/CLASS_NAME/g, className)
+				.replace(/SINGULAR_NAME/g, singularName)
+				.replace(/PLURAL_NAME/g, pluralName);
+
+			fs.writeFileSync(filepath, content);
+
+			log.success('Schema Created', `'${className}' created successfully.`);
+		} else {
+			log.error('Schema Exists', `There is already a schema named '${filename}'`);
+		}
+	} else {
+		log.error('No Name', 'You have to specify schema name');
+	}
+};
