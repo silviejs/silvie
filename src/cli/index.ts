e5c9f80b1bd2b2472961c666745b9412aab01e37
Hossein Maktoobian <8bitsight@gmail.com>
2020-10-19 17:52:59 +0330

    Detect project root in CLI

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
 
-// import path from 'path';
+import fs from 'fs';
+import path from 'path';
 import './path_flags';
 import 'src/bootstrap/paths';
 import log from 'src/utils/log';
 
 // Check to see if it is running in the project root
-// TODO: check silvie existence
-if (process.rootPath !== process.cwd()) {
+if (process.rootPath !== process.cwd() || !fs.existsSync(path.resolve(process.rootPath, 'node_modules/silvie'))) {
 	log.error('[Silvie] Invalid Execution Path');
 	log('Silvie CLI is only accessible from the project root');
+	log('Project root is where silvie package is found under node_modules');
 
 	process.exit();
 }
 
-require('src/bootstrap/configs');
-require('src/bootstrap/dotenv');
 require('src/bootstrap/args');
+require('src/bootstrap/dotenv');
+require('src/bootstrap/configs');
 
 // Import the main cli
 require('./cli');
