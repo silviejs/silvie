5e7ec1490818f1fede59146f9b62536b7052ee1f
Hossein Maktoobian <8bitsight@gmail.com>
2020-10-21 14:30:09 +0330

    Create latlng validation rule

---
+++ src/validator/rules/lat_long.ts
@@ -0,0 +1,12 @@
+import IValidationRule, { rule } from 'src/validator/rule';
+import Validator from 'src/validator';
+import { isLatLong } from 'validator';
+
+@rule('latlng')
+export default class LatLngRule implements IValidationRule {
+	validate(validator: Validator, value: any, checkDMS = 'false'): boolean {
+		return isLatLong(value, {
+			checkDMS: checkDMS && ['yes', 'true', '1'].includes(checkDMS),
+		});
+	}
+}
