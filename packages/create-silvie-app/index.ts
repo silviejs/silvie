a1a02428fe4d2ca148d7094bd0e5335dd90413e4
Hossein Maktoobian <8bitsight@gmail.com>
2020-10-21 14:58:12 +0330

    Rename .npmignore file to .gitignore after copy

--- packages/create-silvie-app/index.ts
+++ packages/create-silvie-app/index.ts
@@ -120,6 +120,13 @@ ncp(path.resolve(__dirname, 'templates/default'), destPath, (error) => {
 		process.exit();
 	}
 
+	try {
+		fs.renameSync(path.resolve(destPath, '.npmignore'), path.resolve(destPath, '.gitignore'));
+	} catch (renameError) {
+		console.error(`Could not create .gitignore file`, renameError);
+		process.exit();
+	}
+
 	console.log('Copied the boilerplate files');
 
 	console.log('Checking project structure...');
