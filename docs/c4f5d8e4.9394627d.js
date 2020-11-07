c941fddefdf2d8431d3d0620d78996b0d3553970
Hossein Maktoobian <8bitsight@gmail.com>
2020-11-07 11:26:02 +0330

    Move sources to /website & website/build folder to /docs

---
+++ docs/c4f5d8e4.9394627d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{78:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),r=t(93);n.default=function(){return o.a.createElement(r.a,{title:"Home Page",description:"A typescript based nodejs framework"})}}}]);
