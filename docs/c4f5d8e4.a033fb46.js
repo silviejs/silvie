38c87c45dd79753022927e3db54d1ee0518a1097
Hossein Maktoobian <8bitsight@gmail.com>
2020-11-15 18:55:47 +0330

    Upload to GHP

---
+++ docs/c4f5d8e4.a033fb46.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{81:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),r=t(105);n.default=function(){return o.a.createElement(r.a,{title:"Home Page",description:"A typescript based nodejs framework"})}}}]);
