195a67856468b9131888ad21de0024f86994a112
Hossein Maktoobian <8bitsight@gmail.com>
2020-12-03 21:23:50 +0330

    Create top section

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import Logo from '../components/logo';
 import styles from './styles.module.scss';
 
 function Home() {
     return (
         <div className={styles.homePage}>
             <Layout title={'Home Page'} description="Silvie Typescript Node.JS Framework">
+                <section className={styles.firstSection}>
+                    <div className={styles.bubbles}>
+                        <Logo className={styles.ts}/>
+                        <Logo className={styles.njs}/>
+                        <Logo className={styles.gql}/>
+                    </div>
 
+                    <h1>Silvie JS</h1>
+                    <p>A <span className={styles.ts}>Typescript</span> Based <span className={styles.njs}>NodeJS</span> Framework</p>
+
+                    <div className={styles.scroll}>
+                        <span>SCROLL</span>
+                    </div>
+                </section>
             </Layout>
         </div>
     );
