f5c1d5237414b94930d0ea16f8523661b394e74a
Hossein Maktoobian <8bitsight@gmail.com>
2020-12-03 12:22:37 +0330

    Create features section

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -15,11 +15,57 @@ function Home() {
                     </div>
 
                     <h1>Silvie JS</h1>
-                    <p>A <span className={styles.ts}>Typescript</span> Based <span className={styles.njs}>NodeJS</span> Framework</p>
+                    <p>A <span className={styles.ts}>Typescript</span> Based <span
+                        className={styles.njs}>NodeJS</span> Framework</p>
 
                     <div className={styles.scroll}>
                         <span>SCROLL</span>
                     </div>
+                </section>
+
+                <section className={styles.featuresSection}>
+                    <h2>More About The Silvie</h2>
+
+                    <section className={styles.features}>
+                        <div className={`${styles.tsready} ${styles.feature}`}>
+                            <img className={styles.illustration} src="/static/img/server-status.svg" alt="Server Status"/>
+
+                            <div className={styles.info}>
+                                <h3>Typescript Ready</h3>
+                                <p>Javascript is awesome, but it lacks some object oriented concepts. That's where
+                                    Typescript comes to serve your needs.</p>
+                            </div>
+                        </div>
+                        <div className={`${styles.graphql} ${styles.feature}`}>
+                            <img className={styles.illustration} src="/static/img/artificial-intelligence.svg"
+                                 alt="Artificial Intelligence"/>
+
+                            <div className={styles.info}>
+                                <h3>GraphQL Integration</h3>
+                                <p>GraphQL came to improve what RESTful APIs were offering. I love it and I wanted Silvie
+                                    developers to simply use it without configuring anything.</p>
+                            </div>
+                        </div>
+                        <div className={`${styles.mvc} ${styles.feature}`}>
+                            <img className={styles.illustration} src="/static/img/folder-searching.svg"
+                                 alt="Folder Structure"/>
+
+                            <div className={styles.info}>
+                                <h3>Well-Structured</h3>
+                                <p>Project structure of a Silvie application is based on MVC and keeps your code base
+                                    organized all the time to reduce confusions.</p>
+                            </div>
+                        </div>
+                        <div className={`${styles.fastdev} ${styles.feature}`}>
+                            <img className={styles.illustration} src="/static/img/moving-forward.svg" alt="Moving Forward"/>
+
+                            <div className={styles.info}>
+                                <h3>Fast Development</h3>
+                                <p>Silvie comes with some built-in features to make it easier for you to concentrate on your
+                                    main application development.</p>
+                            </div>
+                        </div>
+                    </section>
                 </section>
             </Layout>
         </div>
