e3edac25c4e36cf4cd81b7a9626113e7bd914724
Hossein Maktoobian <8bitsight@gmail.com>
2020-12-15 12:24:06 +0330

    Fix nullable validation rule

--- packages/silvie/src/validator/rules/nullable.ts
+++ packages/silvie/src/validator/rules/nullable.ts
@@ -6,11 +6,11 @@ export default class NullableRule implements IValidationRule {
 	validate(validator: Validator, name: string, value: any): boolean {
 		if (value !== null && value !== undefined) {
 			if (typeof value === 'string') {
-				return value.trim().length > 0;
+				return value.trim().length > 0 || undefined;
 			}
 
 			if (value instanceof Array) {
-				return value.length > 0;
+				return value.length > 0 || undefined;
 			}
 
 			return true;
