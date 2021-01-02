d27d2b5f4281979d11af5a104ef687486be8337b
Hossein Maktoobian <8bitsight@gmail.com>
2021-01-02 10:52:12 +0330

    Fix linting error in build script

--- packages/silvie/scripts/build.js
+++ packages/silvie/scripts/build.js
@@ -15,33 +15,34 @@ const rootPath = process.cwd();
 			{ env: { ...process.env, NODE_ENV: 'production' } },
 			async (buildError) => {
 				if (buildError) {
-					return console.log('Build Failed: ', buildError);
+					console.log('Build Failed: ', buildError);
+				} else {
+					console.log('Generating Type Definitions...');
+					childProcess.exec('ttsc', async (typesError) => {
+						if (typesError) {
+							console.log('Build Failed: ', typesError);
+						} else {
+							try {
+								console.log('Copying Package Files...');
+
+								await fs.copyFile(path.resolve(rootPath, 'LICENSE'), path.resolve(rootPath, 'lib/LICENSE'));
+								await fs.copyFile(path.resolve(rootPath, 'README.md'), path.resolve(rootPath, 'lib/README.md'));
+
+								const packageJson = JSON.parse(await fs.readFile(path.resolve(rootPath, 'package.json'), 'utf8'));
+								delete packageJson.scripts;
+								await fs.writeFile(
+									path.resolve(rootPath, 'lib/package.json'),
+									JSON.stringify(packageJson, null, '\t'),
+									'utf8'
+								);
+
+								console.log('Build Finished Successfully');
+							} catch (error) {
+								console.log('Build Failed: ', error);
+							}
+						}
+					});
 				}
-
-				console.log('Generating Type Definitions...');
-				childProcess.exec('ttsc', async (ttscError) => {
-					if (ttscError) {
-						return console.log('Build Failed: ', ttscError);
-					}
-
-					try {
-						console.log('Copying Package Files...');
-						await fs.copyFile(path.resolve(rootPath, 'LICENSE'), path.resolve(rootPath, 'lib/LICENSE'));
-						await fs.copyFile(path.resolve(rootPath, 'README.md'), path.resolve(rootPath, 'lib/README.md'));
-
-						const packageJson = JSON.parse(await fs.readFile(path.resolve(rootPath, 'package.json'), 'utf8'));
-						delete packageJson.scripts;
-						await fs.writeFile(
-							path.resolve(rootPath, 'lib/package.json'),
-							JSON.stringify(packageJson, null, '\t'),
-							'utf8'
-						);
-
-						console.log('Build Finished Successfully');
-					} catch (error) {
-						console.log('Build Failed: ', error);
-					}
-				});
 			}
 		);
 	} catch (error) {
