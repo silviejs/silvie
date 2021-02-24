18b8c5f3b6d3d0cc56f9634e9f9a8c0ee4ec3e8f
Hossein Maktoobian <8bitsight@gmail.com>
2021-02-24 11:01:16 +0330

    Fix is alphanumeric validation rule typo

--- packages/silvie/src/validator/rules/alphanumeric.ts
+++ packages/silvie/src/validator/rules/alphanumeric.ts
@@ -1,10 +1,10 @@
 import IValidationRule, { rule } from 'src/validator/rule';
 import Validator from 'src/validator';
-import { isAlphaNumeric } from 'validator';
+import { isAlphanumeric } from 'validator';
 
 @rule('alphanumeric')
 export default class AlphaNumericRule implements IValidationRule {
 	validate(validator: Validator, name: string, value: any): boolean {
-		return isAlphaNumeric(value);
+		return isAlphanumeric(value);
 	}
 }
